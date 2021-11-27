ccc04879ab3f0e76d0837274d605960c3a5c1735
matheuslanduci <matheuslanduci@gmail.com>
2021-11-27 17:03:05 -0300

    fix: import routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
+import authorsRoutes from './routes/authors'
+import booksRoutes from './routes/books'
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/authors', require('./routes/authors'))
-app.use('/books', require('./routes/books'))
+app.use('/authors', authorsRoutes)
+app.use('/books', booksRoutes)
 
 app.use((err, _req, res, next) => {
   if (err) {
